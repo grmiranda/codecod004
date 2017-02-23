f7e9026604f2ebe9d2b9c86b2c9e7c2c48ed2666
jrnmagalhaes <seuze.rick@gmail.com>
2017-02-23 20:53:12 -0300

    configuração do angularfire2 - conexão com o bando de dados

--- luizDaFeira/src/app/app.module.ts
+++ luizDaFeira/src/app/app.module.ts
@@ -3,13 +3,26 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//angularfire2 - comunicação com o banco de dados e login com face e gmail
+import { AngularFireModule } from 'angularfire2';
+
+// constante de configuração da base de dados
+export const firebaseConfig = {
+  apiKey: "AIzaSyCurTBSL6YJBOTu9axp7DJ28yudC56XiX0",
+  authDomain: "luizdafeira-fd906.firebaseapp.com",
+  databaseURL: "https://luizdafeira-fd906.firebaseio.com",
+  storageBucket: "luizdafeira-fd906.appspot.com",
+  messagingSenderId: "981479929697"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [

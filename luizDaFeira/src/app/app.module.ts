96641564ec3a3dbe3feeea4592644200b9f50ac5
cleybson cardoso <cleybsonc@gmail.com>
2017-02-23 20:45:15 -0300

    adificona module

--- luizDaFeira/src/app/app.module.ts
+++ luizDaFeira/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PushNotificationService } from '../providers/push-notification-service';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { HomePage } from '../pages/home/home';
     MyApp,
     HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [PushNotificationService, {provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}

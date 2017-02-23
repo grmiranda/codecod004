6d4aecd3ea7d8141b16e2889bb6dc08c19ce98a7
cleybson cardoso <cleybsonc@gmail.com>
2017-02-23 20:50:48 -0300

    notificação service

--- luizDaFeira/src/providers/push-notification-service.ts
+++ luizDaFeira/src/providers/push-notification-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 /*
   Generated class for the PushNotificationService provider.
@@ -11,8 +12,21 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PushNotificationService {
 
+  private headers = new Headers({ 'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'Basic NTQ1M2ZhNDUtMzkzNS00YzAzLWFhZTItOWFkZGU0ZjY1ZWQz' });
+
+
   constructor(public http: Http) {
     console.log('Hello PushNotificationService Provider');
+  }
+
+  push(mensagem) {
+    this.http.post('https://onesignal.com/api/v1/notifications', JSON.stringify({
+      "app_id": "04946cb2-d0f6-485b-a390-fea608737a42",
+      "included_segments": ["All"],
+      "headings": { "en": "Luiz da Feira" },
+      "data": { "foo": "bar" },
+      "contents": { "en": mensagem }
+    }), { headers: this.headers }).toPromise().then(res => alert(res)).catch(error => alert("erro ao enviar notificações"));
   }
 
 }

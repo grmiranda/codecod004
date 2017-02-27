c063a8c543c19b41afda05dcd97e7ab05f0757f5
cleybson cardoso <cleybsonc@gmail.com>
2017-02-27 15:16:19 -0300

    face

--- luizDaFeira/src/providers/facebook-service.ts
+++ luizDaFeira/src/providers/facebook-service.ts
@@ -1,39 +1,49 @@
+import { Headers, Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Facebook } from 'ionic-native';
+import { Publicacao } from '../model/publicacao';
+import { User } from '../model/User';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
-import { Facebook } from 'ionic-native';
 
 
-
-/*ffffffffffffffffffffffdsdsdsds
-  Generated class for the FacebookService provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class FacebookService {
 
+  private linkLogin: string = "http://dsoutlet.com.br/igrejaApi/loginFace.php";
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
   constructor(public http: Http) {
-    Facebook.browserInit(1369381413123566, "v2.8");
+    //id do aplicativo: 255509854899402
+    Facebook.browserInit(255509854899402, "v2.8");
   }
 
-  doFbLogin(): Promise<any> {
-
-    return Facebook.login(["public_profile"])
-      .then(function (response) {
-        this.apiFacebook(response);
-    }).catch(error => {
-      alert("Erro ao se logar no facebook");
-    })
+  logar(): Promise<any> {
+    return Facebook.login(["public_profile"]).then(response =>
+      this.api(response, "logar")).catch(this.erro);
   }
 
-  apiFacebook(response, type): Promise<any> {
+  erro() {
+    alert("erro ao tentar se conectar com o servidor");
+  }
+
+  api(response, type): Promise<any> {
     let userID = response.authResponse.userID;
-    return Facebook.api('/' + response.authResponse.userID + '?fields=id,name,gender,email,picture', []).then(result =>{
-      result;
-    });
+    return Facebook.api('/' + response.authResponse.userID + '?fields=id,name,gender,email,picture', []).then(result => {
+      
+      alert(JSON.stringify(result));
+    }
+    );
   }
+
+
+  status(): Promise<any> {
+    return Facebook.getLoginStatus().then(response => response.status);
+  }
+
+  logout(): Promise<any> {
+    return Facebook.logout().then(response => alert("deslogado com Sucesso"));
+  }
+
 
 }

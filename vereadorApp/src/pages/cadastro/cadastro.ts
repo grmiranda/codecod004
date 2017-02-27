1865fef41f16f02009de9cc6b9603a5a4de8fc75
cleybson cardoso <cleybsonc@gmail.com>
2017-02-27 18:13:06 -0300

    ts do cadastro

--- vereadorApp/src/pages/cadastro/cadastro.ts
+++ vereadorApp/src/pages/cadastro/cadastro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { User } from '../../model/user';
 
 /*
   Generated class for the Cadastro page.
@@ -13,10 +14,47 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CadastroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private usuario: User = new User();
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    let loginFace = this.navParams.get("facebook");
+    alert(JSON.stringify(loginFace));
+    this.usuario.nome = loginFace.name;
+    this.usuario.email = loginFace.email;
+    this.usuario.genero = loginFace.gender;
+    this.usuario.fotoURL = loginFace.picture;
+    this.usuario.fbID = loginFace.id;
+
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastroPage');
   }
+
+  private valido(): boolean {
+    if (this.usuario.nome == "") {
+      return false;
+    } else if (this.usuario.email == "") {
+      return false;
+    } else if (this.usuario.nascimento == "") {
+      return false;
+    } else if (this.usuario.telefone == "") {
+      return false;
+    } else if (this.usuario.endereco == "") {
+      return false;
+    } else if (this.usuario.bairro == "") {
+      return false;
+    } else if (this.usuario.cidade == "") {
+      return false;
+    } else if (this.usuario.UF == "") {
+      return false;
+    }
+    return true;
+  }
+
+
 
 }
--- vereadorApp/src/pages/login/login.ts
+++ vereadorApp/src/pages/login/login.ts
@@ -34,7 +34,7 @@ export class LoginPage {
         this.navCtrl.setRoot(HomePage);
       } else {
         //vai para cadastro
-        this.navCtrl.setRoot(CadastroPage);
+        this.navCtrl.setRoot(CadastroPage, {facebook: rese});
       }
     });
   }

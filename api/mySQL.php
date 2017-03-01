d1f6df6f0526aa72777c17a1dd9bd3a96f289f1a
Havallon <havallon.ecomp@gmail.com>
2017-03-01 17:10:55 -0300

    Conectando com o banco

--- api/mySQL.php
+++ api/mySQL.php
@@ -1,2 +1,13 @@
-<?php 
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Content-Type: application/json; charset=UTF-8");
+	$hostname = "localhost";
+	$username = "luizdafeira";
+	$password = "HUf+oR1.J+lI";
+	$database = "luizdafeira";
+	//conectando ao banco
+	$con = mysqli_connect($hostname, $username, $password, $database) or die (mysqli_error());]
+
+    date_default_timezone_set('America/Bahia');
 ?>

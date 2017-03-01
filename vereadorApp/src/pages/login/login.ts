606a717e5ef438292eee8f496e5b72350e17ce41
cleybson cardoso <cleybsonc@gmail.com>
2017-03-01 18:54:45 -0300

    finalizando google

--- vereadorApp/src/pages/login/login.ts
+++ vereadorApp/src/pages/login/login.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { FacebookService } from '../../providers/facebook-service';
-import { FirebaseService } from '../../providers/firebase-service';
 import { HomePage } from '../home/home';
 import { CadastroPage } from '../cadastro/cadastro';
 import { GooglePlusService } from '../../providers/google-plus-service';
@@ -18,7 +17,6 @@ export class LoginPage {
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private facebookService: FacebookService,
-    public firebaseService: FirebaseService,
     private gpService: GooglePlusService,
     private storageService : StorageService
   ) {
@@ -32,15 +30,6 @@ export class LoginPage {
   logar() {
     let idFacebook;
     this.facebookService.logar().then(rese => {
-      alert(JSON.stringify(rese));
-      idFacebook = rese.id;
-      this.firebaseService.fbLogin(idFacebook).then(result => {
-        if (result) {
-          this.navCtrl.setRoot(HomePage);
-        } else {
-          this.navCtrl.push(CadastroPage, { facebook: rese });
-        }
-      })
     });
   }
 

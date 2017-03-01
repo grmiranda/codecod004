79dbb556f40d68bb224f327e6cde9330ebd628f8
cleybson cardoso <cleybsonc@gmail.com>
2017-03-01 17:42:28 -0300

    iniciando face

--- vereadorApp/src/providers/google-plus-service.ts
+++ vereadorApp/src/providers/google-plus-service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { GoogleAuth, User } from '@ionic/cloud-angular';
+import 'rxjs/add/operator/toPromise';
 
 /*
   Generated class for the GooglePlusService provider.
@@ -11,8 +13,33 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GooglePlusService {
 
-  constructor(public http: Http) {
+  private link : string = "http://dsoutlet.com.br/apiLuiz/cadastro.php";
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+
+  constructor(
+    public http: Http,
+    public googleAuth: GoogleAuth,
+    public user: User
+  ) {
     console.log('Hello GooglePlusService Provider');
+  }
+
+  public loginGoogle(): Promise<any> {
+    return this.googleAuth.login().then(sucess => {
+      alert(JSON.stringify(sucess));
+      let full_name = this.user.social.google.data.full_name;
+      let profile_picture = this.user.social.google.data.profile_picture;
+      this.http.post(this.link, JSON.stringify({full_name, profile_picture, sucess})).toPromise().then(res=>{
+        
+      }).catch(()=>alert("Erro ao se conectar com o servidor"));
+
+    }).catch(() => alert("Erro ao se conectar com o google plus"));
+  }
+
+
+  public logoutGoogle() {
+    alert("Deslogado com sucesso");
+    this.googleAuth.logout();
   }
 
 }

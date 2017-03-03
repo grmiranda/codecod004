165ebbb8107b7374671212853ae7faf64ae8290f
lucas paiva <lukspaiva95@gmail.com>
2017-03-03 18:25:55 -0300

    avaliando solicitacoes

--- vereadorApp/src/app/app.component.ts
+++ vereadorApp/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { AvaliarSolicitacaoPage } from '../pages/avaliar-solicitacao/avaliar-sol
 })
 export class MyApp {
   @ViewChild(Nav) navCtrl: Nav;
-  rootPage = LoginPage;
+  rootPage = HomePage;
   pages: Array<{ title: string, component: any }>;
   pageAtual: string;
 

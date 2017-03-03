d928d5544386e408eb31cadc84bffdb559ad6a74
lucas paiva <lukspaiva95@gmail.com>
2017-03-03 18:47:05 -0300

    avaliando solicitacoes

--- vereadorApp/src/pages/solic-aprovados/solic-aprovados.ts
+++ vereadorApp/src/pages/solic-aprovados/solic-aprovados.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { SolicitacaoService } from '../../providers/solicitacao-service';
+import { Solicitacao } from '../../model/solicitacao';
 
-/*
-  Generated class for the SolicAprovados page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-solic-aprovados',
   templateUrl: 'solic-aprovados.html'
 })
 export class SolicAprovadosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private solicitacoes: Solicitacao[] = [];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SolicAprovadosPage');
-  }
+  constructor(public solicitacaoService: SolicitacaoService) { }
+
 
 }
--- vereadorApp/src/pages/solic-solicitados/solic-solicitados.ts
+++ vereadorApp/src/pages/solic-solicitados/solic-solicitados.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { SolicitacaoService } from '../../providers/solicitacao-service';
+import { Solicitacao } from '../../model/solicitacao';
 
-/*
-  Generated class for the SolicSolicitados page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-solic-solicitados',
   templateUrl: 'solic-solicitados.html'
 })
 export class SolicSolicitadosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private solicitacoes: Solicitacao[] = [];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SolicSolicitadosPage');
+  constructor(public solicitacaoService: SolicitacaoService) { }
+
+  ionViewWillEnter() {
+    this.carregarSolicitacoes();
   }
 
+  private carregarSolicitacoes() {
+    this.solicitacaoService.getSolicitacoes('sl').then(res => {
+      if (!res.error) {
+        this.solicitacoes = res.data;
+      }
+    })
+  }
 }

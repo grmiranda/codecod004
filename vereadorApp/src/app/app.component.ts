1dca6601b1e7a74b62e3c361913ea2df4f9c8969
lucas paiva <lukspaiva95@gmail.com>
2017-03-05 00:43:40 -0300

    parte chata de cley

--- vereadorApp/src/app/app.component.ts
+++ vereadorApp/src/app/app.component.ts
@@ -47,16 +47,16 @@ export class MyApp {
       var notificationOpenedCallback = function (jsonData) {
       };
 
-      window["plugins"].OneSignal
-        .startInit("04946cb2-d0f6-485b-a390-fea608737a42")
-        .handleNotificationOpened(notificationOpenedCallback)
-        .endInit();
+      // window["plugins"].OneSignal
+      //   .startInit("04946cb2-d0f6-485b-a390-fea608737a42")
+      //   .handleNotificationOpened(notificationOpenedCallback)
+      //   .endInit();
 
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
 
-    window["plugins"].OneSignal
+    //window["plugins"].OneSignal
   }
 
   openPage(page) {

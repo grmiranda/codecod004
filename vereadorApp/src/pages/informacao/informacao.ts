671b7c33f9917f57b20f500595aeeaf5ac3457db
cleybson cardoso <cleybsonc@gmail.com>
2017-03-09 23:57:21 -0300

    pegando

--- vereadorApp/src/pages/informacao/informacao.ts
+++ vereadorApp/src/pages/informacao/informacao.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { Questoes } from '../../model/Questoes';
 
 /*
   Generated class for the Informacao page.
@@ -13,10 +15,33 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class InformacaoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private link: string = "http://dsoutlet.com.br/apiLuiz/getInformacoes.php?info";
+  private questoes: Questoes[];
+  private questaoEscolhida = new Questoes();
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private http: Http
+    ) {
+      
+      this.http.get(this.link).toPromise().then(res=>{
+        this.questoes = res.json();
+      }).catch(()=>alert("Erro ao se comunicar com o servidor"));
+      
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InformacaoPage');
   }
+
+  escolherPergunta(pergunta){
+    if(this.questaoEscolhida == pergunta){
+      this.questaoEscolhida = new Questoes();
+    } else{
+      this.questaoEscolhida = pergunta;
+    }
+  }
+
 
 }

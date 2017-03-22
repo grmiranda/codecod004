d23184c69f8d087e310e57affa5ea23278aa995d
jrnmagalhaes <seuze.rick@gmail.com>
2017-03-22 14:52:17 -0300

    tela de avaliar depoimentos

--- vereadorApp/src/pages/avaliar-depoimento/avaliar-depoimento.ts
+++ vereadorApp/src/pages/avaliar-depoimento/avaliar-depoimento.ts
@@ -1,22 +1,55 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController, NavController, NavParams } from 'ionic-angular';
+import { Depoimento } from '../../model/depoimento';
 
-/*
-  Generated class for the AvaliarDepoimento page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-avaliar-depoimento',
   templateUrl: 'avaliar-depoimento.html'
 })
 export class AvaliarDepoimentoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private depoimentos: Depoimento[] = [];
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public actionSheetCtrl: ActionSheetController) {
+
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AvaliarDepoimentoPage');
+  }
+
+  private abrirOpcoes(depoimento: any) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Opções',
+      buttons: [
+        {
+          text: 'Negar',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Aprovar',
+          icon: 'checkmark-circle',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
   }
 
 }

f0336e7c8e23d16148fef4d25701596d91db8c6b
Havallon <havallon.ecomp@gmail.com>
2017-03-23 14:25:01 +0000

    finish users.php

--- api/users.php
+++ api/users.php
@@ -61,6 +61,47 @@
                 				$vetor[] = $temp;
 					}
 					echo json_encode($vetor);
+				} else {
+					$sql = "SELECT * FROM usuario WHERE IDUsuario != '$id' AND permissao = '1'";
+					$result = $con->query($sql);
+					
+					while($row=$result->fetch_assoc()){
+						$userID = $row['IDUsuario'];
+						
+						$sql = "SELECT * FROM telefone WHERE IDUsuario = '$userID'";
+						$resultado = $con->query($sql);
+						$telefone = $resultado->fetch_assoc();
+						
+						$sql = "SELECT * FROM endereco WHERE IDUsuario = '$userID'";
+						$resultado = $con->query($sql);
+						$end = $resultado->fetch_assoc();
+						
+						if ($row['genero'] == 'm') {
+							$row['genero'] = 'male';
+						} else {
+							$row['genero'] = 'female';
+						}
+						
+						$temp = array();
+
+                				$temp['IDUsuario'] = $row['IDUsuario'];
+               					$temp['nome'] = $row['nome'];
+                				$temp['email'] = $row['email'];
+                				$temp['genero'] = $row['genero'];
+                				$temp['fotoURL'] = $row['fotoURL'];
+                				$temp['socialID'] = $row['socialID'];
+               				 	$temp['cpf'] = $row['cpf'];
+                				$temp['nascimento'] = $row['nascimento'];
+						$temp['telefone'] = $telefone['numero'];
+						$temp['endereco'] = $end['endereco'];
+						$temp['bairro'] = $end['bairro'];
+						$temp['cidade'] = $end['cidade'];
+						$temp['UF'] = $end['uf'];
+						$temp['Push'] = $row['Push'];
+
+                				$vetor[] = $temp;
+					}
+					echo json_encode($vetor);
 				}
 			}
 		}

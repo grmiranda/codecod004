e0ca1ce49b05f4830d5b3e5f85a57d5297aa3660
Havallon <havallon.ecomp@gmail.com>
2017-03-23 14:42:35 -0300

    refresh.php

--- api/refresh.php
+++ api/refresh.php
@@ -5,16 +5,12 @@
 
 <?php 
 	
-	$dia = date('j');
-	
-	$hora = date('H:i');
-	
-	if ($dia == 1 && $hora == "00:00") {
-		$sql = " UPDATE usuario SET pontos = '0' ";
-		$con->query($sql);
+
+	$sql = " UPDATE usuario SET pontos = '0' ";
+	$con->query($sql);
 		
-		echo json_encode(true);
-	}
+	echo json_encode(true);
+
 	
 	$con->close();
 

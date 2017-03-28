dc46f582de0d681bbbe40deac30b049048019e6c
Havallon <havallon.ecomp@gmail.com>
2017-03-28 19:07:07 -0300

    publicacoes com varias fotos

--- api/getPublicacoes.php
+++ api/getPublicacoes.php
@@ -9,8 +9,19 @@
 		if ($_GET["id"] == ""){
 			$sql = "SELECT * FROM publicacao ORDER BY IDPublicacao DESC";
 			$result = $con->query($sql);
+
 			while($row=$result->fetch_assoc()){
+				$id = $row['IDPublicacao'];
+
+				$sql = "SELECT * FROM fotourl WHERE id = '$id' AND tipo = 'publicacao'";
+				$resultado = $con->query($sql);
+				$fotos = array();
+				while ($f=$resultado->fetch_assoc()){
+					$fotos[] = $f;
+				}
+
 				$vetor[] = $row;
+				$vetor['fotoURL'] = $fotos;
 			}
 			echo json_encode($vetor);
 		}

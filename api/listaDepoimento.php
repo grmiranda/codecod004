0996f7c55ef9fa02f75db5fa8b400d94b3b7b9dc
Lucas Paiva <lukspaiva95@gmail.com>
2017-03-30 16:40:41 -0300

    lista depoimento php

--- api/listaDepoimento.php
+++ api/listaDepoimento.php
@@ -10,7 +10,7 @@
 			$tipo = $_GET["tipo"];
 			
 			if ($tipo == "ap") {
-				$sql = "SELECT d.IDDepoimento, d.Texto, u.nome, u.fotoURL FROM depoimento d LEFT JOIN usuario u ON (d.IDUsuario = u.IDUsuario) WHERE estado ='ap' ";
+				$sql = "SELECT d.IDDepoimento, d.Texto, u.nome, u.fotoURL FROM depoimento d LEFT JOIN usuario u ON (d.IDUsuario = u.IDUsuario) WHERE estado ='ap' ORDER BY dataAceitacao DESC";
 				$result = $con->query($sql);
 				
 				while($row=$result->fetch_assoc()){

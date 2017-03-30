eda2c55d97fe2ee17669a576184b7fccb0b04e02
Lucas Paiva <lukspaiva95@gmail.com>
2017-03-30 13:52:00 -0300

    tratamento de erros

--- vereadorApp/src/pages/historia/historia.ts
+++ vereadorApp/src/pages/historia/historia.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Historia page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-historia',
   templateUrl: 'historia.html'
 })
 export class HistoriaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HistoriaPage');
-  }
+  constructor() {}
 
 }
--- vereadorApp/src/pages/modal-opcoes/modal-opcoes.ts
+++ vereadorApp/src/pages/modal-opcoes/modal-opcoes.ts
@@ -1,32 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 
-/*
-  Generated class for the ModalOpcoes page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-modal-opcoes',
   templateUrl: 'modal-opcoes.html'
 })
 export class ModalOpcoesPage {
-  opcoes = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController) {
+
+  private opcoes = [];
+
+  constructor(public navParams: NavParams, public view: ViewController) {
     this.opcoes = this.navParams.get('opcoes');
-
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalOpcoesPage');
-  }
-
-  cancel() {
+  private cancel() {
     this.view.dismiss()
   }
 
-  acao(opcao){
+  private acao(opcao) {
     this.view.dismiss(opcao);
   }
 }

74889073aedd54a2d1b43c1722ea6c1d7a0ef5c3
cleybson cardoso <cleybsonc@gmail.com>
2017-04-28 20:31:05 -0300

    pegando

--- api/getSolicitacaoId.php
+++ api/getSolicitacaoId.php
@@ -9,17 +9,18 @@
 		$id = $_GET["id"];
 		$sql = "SELECT * FROM solicitacao WHERE IDSolicitacao = '$id'";
 		$result = $con->query($sql);
-		$numrow = $result->num_rows;
-		if($numrow == 1){
-			$row=$result->fetch_assoc();
-			$sql = "SELECT * FROM fotourl WHERE id = '$id' AND tipo = 'solicitacao'";
-			$resultado = $con->query($sql);
-			$fotos = array();
-			while ($f=$resultado->fetch_assoc()){
-				$fotos[] = $f['fotoURL'];
-			}
-			$row['fotoURL'] = $fotos;
+		$row=$result->fetch_assoc();
+		$sql = "SELECT * FROM fotourl WHERE id = '$id' AND tipo = 'solicitacao'";
+		$resultado = $con->query($sql);
+		$fotos = array();
+		while ($f=$resultado->fetch_assoc()){
+			$fotos[] = $f['fotoURL'];
 		}
+		$row['fotoURL'] = $fotos;
+		
+		$sql = "SELECT * FROM apoiosolicitacao WHERE IDSolicitacao = '$id' ";
+		$resultado = $con->query($sql);
+		
 		echo json_encode($row);
 	} 	
 	$con->close();	
